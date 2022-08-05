b7ba3b4f5274a03e0606738dc18365ccb865c008
WeakPlayer-rgb <kkishun00@gmail.com>
2022-08-05 14:34:27 +0500

    Fix som issues

---
+++ .net_server/Solution1/Server/Entities/Payment/Payment.cs
@@ -0,0 +1,18 @@
+﻿namespace Server.Entities.Payment;
+
+public class Payment
+{
+    public Payment()
+    {
+    }
+    public Payment(int id, Guid userId)
+    {
+        Id = id;
+        UserId = userId;
+    }
+    public int Id { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime PaymentDateTime { get; set; }
+
+    public bool Confirm { get; set; } = false;
+}
